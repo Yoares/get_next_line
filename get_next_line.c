c3b0821c4c80f1d813405fffe87b33650802096d
Yasser Khoussi <ykhoussi@e2r8p2.1337.ma>
2024-12-05 21:38:04 +0100

    hoooow

--- get_next_line.c
+++ get_next_line.c
@@ -6,98 +6,104 @@
 /*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:17:00 by ykhoussi          #+#    #+#             */
-/*   Updated: 2024/12/05 21:11:51 by ykhoussi         ###   ########.fr       */
+/*   Updated: 2024/12/05 21:35:22 by ykhoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *read_manage(int fd, char *full_line)
+char	*read_manage(int fd, char *full_line)
 {
-    char *buffer;
-    ssize_t byte_read;
-    char *temp;
+	char	*buffer;
+	ssize_t	byte_read;
+	char	*temp;
 
-    while (1)
-    {
-        buffer = ft_calloc(BUFFER_SIZE +1 , 1);
-        if (!buffer)
-            return(NULL);
-        byte_read = read(fd, buffer, BUFFER_SIZE);
-        if (byte_read <= 0)
-        {
-            free(buffer);
-            break;
-        }
-        buffer[byte_read] = '\0';
-        temp = full_line;
-        full_line = ft_strjoin(full_line, buffer);
-        free(buffer);
-        if (temp)
-            free(temp);
-        if (ft_strchr(buffer, '\n'))
-            break;
-    }
-    return (full_line);
-}
-char *return_line(char *full_line)
-{
-    size_t i;
-    char    *line;
-    
-    i = 0;
-    while(full_line[i] && full_line[i] != '\n')
-        i++;
-    line = ft_calloc(i + 1 + (full_line[i] == '\n'), sizeof(char));
-    if (!line)
-        return (NULL);
-    ft_memcpy(line, full_line, i + (full_line[i] == '\n'));
-    return (line);
-}
-int checknl(char *s )
-{
-    int i = 0 ;
-    while(s[i] != '\n' && s[i])
-    {
-        i++;
-    }
-    return i + (s[i] == '\n');
-}
-char *trim(char *s, int i)
-{
-    char *ns;
-    int len = ft_strlen(s) - i;
-
-    if (len <= 0)
-    {
-        free(s);
-        return (NULL);
-    }
-    ns = ft_calloc(len + 1, 1);
-    if (!ns)
-        return (NULL);
-    ft_memcpy(ns, s + i, len);
-    free(s);
-    return (ns);
+	while (1)
+	{
+		buffer = ft_calloc(BUFFER_SIZE + 1, 1);
+		if (!buffer)
+			return (NULL);
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read <= 0)
+		{
+			free(buffer);
+			break ;
+		}
+		buffer[byte_read] = '\0';
+		temp = full_line;
+		full_line = ft_strjoin(full_line, buffer);
+		free(buffer);
+		if (temp)
+			free(temp);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (full_line);
 }
 
-char *get_next_line(int fd)
+char	*return_line(char *full_line)
 {
-    static  char *full_line;
-    char    *new_line;
-    
-    if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
-        return (NULL);
-    full_line = read_manage(fd, full_line);
-    if (!full_line)
-        return (NULL);
-    if (full_line)
-        new_line = return_line(full_line);
-    full_line  = trim(full_line , checknl(full_line));
-    if (!new_line && !full_line)
-    {
-        free(full_line);
-        full_line = NULL;
-    }
-    return (new_line);
+	size_t	i;
+	char	*line;
+
+	i = 0;
+	while (full_line[i] && full_line[i] != '\n')
+		i++;
+	line = ft_calloc(i + 1 + (full_line[i] == '\n'), sizeof(char));
+	if (!line)
+		return (NULL);
+	ft_memcpy(line, full_line, i + (full_line[i] == '\n'));
+	return (line);
+}
+
+int	checknl(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\n' && s[i])
+	{
+		i++;
+	}
+	return (i + (s[i] == '\n'));
+}
+
+char	*trim(char *s, int i)
+{
+	char	*ns;
+	int		len;
+
+	len = ft_strlen(s) - i;
+	if (len <= 0)
+	{
+		free(s);
+		return (NULL);
+	}
+	ns = ft_calloc(len + 1, 1);
+	if (!ns)
+		return (NULL);
+	ft_memcpy(ns, s + i, len);
+	free(s);
+	return (ns);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*full_line;
+	char		*new_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
+		return (NULL);
+	full_line = read_manage(fd, full_line);
+	if (!full_line)
+		return (NULL);
+	if (full_line)
+		new_line = return_line(full_line);
+	full_line = trim(full_line, checknl(full_line));
+	if (!new_line && !full_line)
+	{
+		free(full_line);
+		full_line = NULL;
+	}
+	return (new_line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,24 +6,24 @@
 /*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:14:40 by ykhoussi          #+#    #+#             */
-/*   Updated: 2024/12/05 14:03:13 by ykhoussi         ###   ########.fr       */
+/*   Updated: 2024/12/05 21:37:33 by ykhoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t   ft_strlen(char *s1)
+size_t	ft_strlen(char *s1)
 {
-    size_t i;
-	
-    if (!s1)
-        return (0);    
-    i = 0;
-    while (s1[i])
-    {
-        i++;
-    } 
-    return (i);
+	size_t	i;
+
+	if (!s1)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		i++;
+	}
+	return (i);
 }
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
@@ -45,28 +45,28 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    size_t s1_len;
-    size_t s2_len;
-    char *ptr;
-    
-    if (!s1 && !s2)
-        return (NULL);
-    s1_len = 0;
-    s2_len = 0;
-    if (s1 != NULL)
-        s1_len = ft_strlen(s1);
-    if (s2 != NULL)
-        s2_len = ft_strlen(s2);
-    ptr = malloc(s1_len + s2_len + 1);
-    if (!ptr)
-        return (NULL);
-    if (s1)
-        ft_memcpy(ptr, s1, s1_len);
-    if (s2)
-        ft_memcpy(ptr + s1_len, s2, s2_len);
-    return (ptr);
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*ptr;
+
+	if (!s1 && !s2)
+		return (NULL);
+	s1_len = 0;
+	s2_len = 0;
+	if (s1 != NULL)
+		s1_len = ft_strlen(s1);
+	if (s2 != NULL)
+		s2_len = ft_strlen(s2);
+	ptr = malloc(s1_len + s2_len + 1);
+	if (!ptr)
+		return (NULL);
+	if (s1)
+		ft_memcpy(ptr, s1, s1_len);
+	if (s2)
+		ft_memcpy(ptr + s1_len, s2, s2_len);
+	return (ptr);
 }
 
 void	*ft_calloc(size_t count, size_t size)
@@ -87,12 +87,13 @@ void	*ft_calloc(size_t count, size_t size)
 	}
 	return (s);
 }
+
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
 
-    if (!s)
-        return (NULL);
+	if (!s)
+		return (NULL);
 	i = 0;
 	while (s[i])
 	{

e8758dd8a25f4a7b678a7ed5f10b1abf75ac1e9e
Yasser Khoussi <ykhoussi@e2r4p12.1337.ma>
2024-12-07 23:25:13 +0100

    hello

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/28 15:15:10 by ykhoussi          #+#    #+#             */
-/*   Updated: 2024/12/07 22:01:42 by ykhoussi         ###   ########.fr       */
+/*   Created: 2024/12/07 22:01:51 by ykhoussi          #+#    #+#             */
+/*   Updated: 2024/12/07 23:10:50 by ykhoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-# include <stdlib.h>
 # include <unistd.h>
-# include <stdint.h>
+# include <stdlib.h>
+#include <fcntl.h>
+#include <stdint.h>
 # include <limits.h>
-# include <stdio.h>
-# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1024
+#  define BUFFER_SIZE 1024
 # endif
+
+# define MAX_FD 1024
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
@@ -30,4 +31,5 @@ void	*ft_calloc(size_t count, size_t size);
 size_t	ft_strlen(char *s1);
 char	*ft_strchr(const char *s, int c);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+
 #endif
